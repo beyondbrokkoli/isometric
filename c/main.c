d93cc4b9585b892a6ddfa291d00d85263fb51a32
beyondbrokkoli <halim.zag@gmx.de>
2026-05-29 11:32:27 +0200

    m

--- c/main.c
+++ c/main.c
@@ -198,7 +198,7 @@ void glfw_key_callback(GLFWwindow* window, int key, int scancode, int action, in
         }
     }
     if (action == GLFW_PRESS) {
-        if (key == GLFW_KEY_1 || key == GLFW_KEY_2 || key == GLFW_KEY_3 || key == GLFW_KEY_3 || key == GLFW_KEY_F5) {
+        if (key == GLFW_KEY_1 || key == GLFW_KEY_2 || key == GLFW_KEY_3 || key == GLFW_KEY_4 || key == GLFW_KEY_F5) {
             atomic_store_explicit(&g_engine.mailbox.last_key_pressed, key, memory_order_release);
         }
     }
