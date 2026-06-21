d55e9f77aa69869269a9d58e3ef4f17f85c83339
beyondbrokkoli <halim.zag@gmx.de>
2026-06-21 21:25:07 +0200

    m

--- lua/network.lua
+++ lua/network.lua
@@ -6,8 +6,8 @@ ffi.cdef[[
     int vx_net_connect(uint8_t peer_id, const char* ip, int port);
     void vx_net_set_session(uint64_t token);
     void vx_net_set_player_id(uint8_t id);
-    void vx_net_send_to(void* pkt, uint8_t target_peer);
     int vx_net_recv_all(void* out_buffer, int max_count);
+    void vx_net_send_to(void* data, size_t len, uint8_t target_peer);
     void vx_net_set_relay_ip(const char* ip);
     uint32_t vx_net_hash_state(const void* data, size_t length, uint32_t initial_hash);
     int vx_net_stun_punch(const char* stun_server_ip, int stun_port, char* out_ip, int* out_port);
